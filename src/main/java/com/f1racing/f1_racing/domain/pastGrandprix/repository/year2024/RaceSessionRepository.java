9f164b3ae1cb8b94d3feb998b3c4526b26b0b5dc
yewonniy <ae6652@naver.com>
2026-01-23 20:42:49 +0900

    feat: 그랑프리 찐 출발시간 계산하는 api 추가

--- src/main/java/com/f1racing/f1_racing/domain/pastGrandprix/repository/year2024/RaceSessionRepository.java
+++ src/main/java/com/f1racing/f1_racing/domain/pastGrandprix/repository/year2024/RaceSessionRepository.java
@@ -1,8 +1,10 @@
 package com.f1racing.f1_racing.domain.pastGrandprix.repository.year2024;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+
 import com.f1racing.f1_racing.domain.pastGrandprix.entity.year2024.RaceSession24;
 
 public interface RaceSessionRepository extends JpaRepository<RaceSession24, Integer> {
-
+    RaceSession24 findBySessionKey(Integer sessionKey);
 }
--- src/main/java/com/f1racing/f1_racing/domain/pastGrandprix/repository/year2025/RaceSession25Repository.java
+++ src/main/java/com/f1racing/f1_racing/domain/pastGrandprix/repository/year2025/RaceSession25Repository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.f1racing.f1_racing.domain.pastGrandprix.entity.year2025.RaceSession25;
 
 public interface RaceSession25Repository extends JpaRepository<RaceSession25, Integer>{
-
+    RaceSession25 findBySessionKey(Integer sessionKey);
 }
