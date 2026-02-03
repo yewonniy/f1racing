7d61327772a84db2669770b92aa16d2a4e1bb3da
yewonniy <ae6652@naver.com>
2026-02-03 12:03:02 +0900

    fix: WebConfig에 프론트 배포 주소 추가

--- src/main/java/com/f1racing/f1_racing/global/config/WebConfig.java
+++ src/main/java/com/f1racing/f1_racing/global/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 API 주소에 대해서
                 .allowedOrigins(
                         "http://localhost:5173", // 로컬 프론트엔드 주소 허용
-                        "http://127.0.0.1:5173"  // 혹시 모를 IP 접속 허용
+                        "http://127.0.0.1:5173",  // 혹시 모를 IP 접속 허용
+                        "https://f1racing-frontend.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
